e262cb3e369cb3dc88689e1c12f4f7a6d62092f3
handabaldeep <handabaldeep@gmail.com>
2019-07-14 21:32:32 +0100

    Added ML models

---
+++ Week-1/kMeans.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 20:16:52 2019
+
+@author: handabaldeep
+"""
+import random
+import numpy as np
+import sys
+
+tolerance = 0.01
+max_iterations = 10
+
+def dist(a,b):
+    _sum = 0
+    for i in range(len(a)):
+        _sum += (a[i] - b[i])**2
+    return _sum**0.5
+
+def centroid_dist(cent,p):
+    min_dist = sys.maxsize
+    pos = -1
+    for i in range(len(cent)):
+        #d = dist(p,cent[i])
+        d = abs(p-cent[i])
+        if d < min_dist:
+            min_dist = d
+            pos = i+1
+    return pos
+
+def kmeans(input_arr,k=3):
+    k_values = np.zeros(input_arr.shape[0],dtype=int)
+    for i in range(len(k_values)):
+        k_values[i] = random.randint(1,k)
+        
+    c = sorted(zip(k_values,input_arr))
+    print(c)
+    
+    centroids = np.zeros(k)
+    for j in range(max_iterations):
+        k,i = 0,0
+        while i<len(c):
+            _sum,num = 0,0
+            while i<len(c) and c[i][0] == k+1:
+                _sum += c[i][1]
+                num += 1
+                i += 1
+            centroids[k] = _sum/num
+            #print(centroids[k],_sum,num,k)
+            k += 1
+        
+        #print(centroids)
+        for i in range(len(c)):
+            c[i] = (centroid_dist(centroids,c[i][1]),c[i][1])
+        
+        c.sort()
+        #print(c)
+    return c
+        
+a = np.array([1,2,3,4,5,6,7,8,9,10])
+print(kmeans(a))
